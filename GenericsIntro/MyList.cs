169a6a06695ec4624f7da9594d65e37e22222966
Tubayldrm <yildirimtugba18@gmail.com>
2021-12-15 12:31:08 +0300

    15 Aralık ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] İtems;
+        public MyList()
+        {
+            İtems = new T[0];
+        }
+        public void Add(T İtem)
+        {
+            T[] tempArray = İtems;
+            İtems = new T[İtems.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                İtems[i] = tempArray[i];
+            }
+
+            İtems[İtems.Length - 1] = İtem;
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> İsimler = new MyList<string>();
+            İsimler.Add("Tuba");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
