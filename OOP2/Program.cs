d1e8251711e85ad8eb986e7168352701e907022e
Tubayldrm <yildirimtugba18@gmail.com>
2021-12-17 03:43:58 +0300

    17 Aralık ders sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Tuba Yıldırım
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Tuba";
+            musteri1.Soyadi = "Yıldırım";
+            musteri1.TcNo = "12345678910";
+
+
+            //kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+
+
+
+
+       
+           
+        }
+    }
+}
